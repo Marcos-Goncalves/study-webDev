c9f67ccfa0b51ad633b5100c0870059190efe73a
Marcos Gonçalves <malxg@Jarvis.linux>
2022-03-31 21:45:57 -0300

    alterando a rota

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/login', function(){return 'Login';});
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtoss', function(){return 'Produtos';})->name('app.produtos');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
