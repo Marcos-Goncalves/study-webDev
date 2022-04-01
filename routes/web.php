9ea9164bc2669ef167269f79d1020ab009641205
Marcos Gonçalves <malxg@Jarvis.linux>
2022-03-31 21:40:34 -0300

    mudei a rota

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/login', function(){return 'Login';});
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
     Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
-    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+    Route::get('/produtoss', function(){return 'Produtos';})->name('app.produtos');
 });
 
 Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
@@ -37,3 +37,4 @@ Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste')
 Route::fallback(function(){
     return 'Pagina inexistente, <a href="'.route('site.index').'">clique aqui</a> para voltar.';
 });
+
